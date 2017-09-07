4889967819bdb48d78e982216093248e6c312e5a
Smita Acharya <sacharya@tavisca.com>
2017-09-07 21:10:56 +0530

    added object serializer file

---
+++ serializeobj/Clientsideapp/Clientsideapp/Client.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace Clientside
+{
+    class Client
+    {
+        static void Main(string[] args)
+        {
+            Person P = new Person();
+            var data = JsonConvert.SerializeObject(P);
+            TcpClient client = new TcpClient("172.16.14.124", 8080);
+            Console.WriteLine("Connected to server");
+            NetworkStream n = client.GetStream();
+            Console.WriteLine("connected");
+          
+            byte[] message = Encoding.Unicode.GetBytes(data);
+            n.Write(message, 0, message.Length);
+            Console.WriteLine("message sent");
+            client.Close();
+            Console.ReadKey();
+
+
+        }
+    }
+}
